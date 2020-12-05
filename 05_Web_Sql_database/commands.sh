6e74d52211df4c3949553f0b184d778e1ef77c1d
Houssem Dellai <houssem.dellai@live.com>
2020-12-05 19:26:56 +0100

    added show commands

--- 05_Web_Sql_database/commands.sh
+++ 05_Web_Sql_database/commands.sh
@@ -10,6 +10,9 @@ terraform plan -out tfplan
 # show the tfplan file
 terraform show -json tfplan
 terraform show -json tfplan >> tfplan.json
+# show only the changes
+cat tfplan.json | jq '[.resource_changes[] | {type: .type, name: .change.after.name, actions: .change.actions[]}]' 
+cat tfplan.json | jq -r '(.resource_changes[] | [.change.actions[], .type, .change.after.name]) | @tsv'
 
 # apply the infra changes
 terraform apply tfplan
