6f3126483c7cc268317a2c102adf97a35dfb0cd0
Houssem Dellai <houssem.dellai@live.com>
2020-12-18 16:34:29 +0100

    added features 20_

--- 20_tf_features/main.tf
+++ 20_tf_features/main.tf
@@ -1,5 +1,8 @@
+##############################################
+# locals, lower
+##############################################
+
 locals {
-  # Common tags to be assigned to all resources
   common_tags = {
     CostCenter = var.cost_center_id
     Production = var.is_production
@@ -7,11 +10,15 @@ locals {
 }
 
 resource "azurerm_resource_group" "global_rg" {
-  name     = var.rg_name
+  name     = lower(var.rg_name)
   location = var.location
 
   tags = local.common_tags
 }
+
+##############################################
+# for_each
+##############################################
 
 resource "azurerm_resource_group" "env_rg" {
   for_each = {
@@ -22,13 +29,21 @@ resource "azurerm_resource_group" "env_rg" {
   name     = each.key
   location = each.value
 }
+
 ##############################################
+# count
+##############################################
+
 resource "azurerm_resource_group" "apps_rg" {
   count    = 3
   name     = "app-${count.index}"
   location = "westeurope"
 }
+
 ##############################################
+# count, list
+##############################################
+
 variable "apps_names" {
   type    = list(string)
   default = ["frontend", "backend", "database"]
@@ -39,3 +54,14 @@ resource "azurerm_resource_group" "org_apps_rg" {
   name     = var.apps_names[count.index]
   location = "westeurope"
 }
+
+##############################################
+# if, else
+##############################################
+
+resource "azurerm_resource_group" "dev_rg" {
+  name                  = "dev-rg"
+  location              = var.location != "" ? var.location : "westeurope"
+}
+
+# managed_disk_type = var.environment == "Dev" ? "Standard_LRS" : "Premium_LRS"
--- 20_tf_features/variables.tf
+++ 20_tf_features/variables.tf
@@ -1,3 +1,7 @@
+##############################################
+# string, number, list, map
+##############################################
+
 variable "cost_center_id" {
   type    = number
   default = 3
@@ -22,6 +26,10 @@ variable "custom_tags" {
   }
 }
 
+##############################################
+# sensitive, validation, length, substr
+##############################################
+
 variable "rg_name" {
   type        = string
   description = "The name of the Azure Resource Group."
@@ -34,6 +42,10 @@ variable "rg_name" {
     error_message = "The rg_name value must be valid, starting with \"rg-\"."
   }
 }
+
+##############################################
+# anytrue, for, contains
+##############################################
 
 variable "location" {
   type        = string
