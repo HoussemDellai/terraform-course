203c8f4b968e622382b276565db2cc92565bc175
Houssem Dellai <houssem.dellai@live.com>
2021-09-26 08:19:49 +0200

    added sa to import

--- 90_import_resource/commands.sh
+++ 90_import_resource/commands.sh
@@ -1,14 +1,22 @@
 # demo for importing existing infrastructure into Terraform configuration files
-# create an Azure Resource Group
+# create an Azure Resource Group and a Storage Account
 az group create -g myResourceGroup -l westeurope
+az storage account create -g myResourceGroup -n tfsademo01
 
-# we use the main.tf where we created the tf resource for the resource group
+# in main.tf we created the tf resource for the resource group & storage account
 
 # init terraform's Azure provider
 terraform init
 
 # import the above created resource group into terraform state file
-terraform import azurerm_resource_group.rg '/subscriptions/<SUBSCRIPTION_ID>/resourceGroups/myResourceGroup'
+# replace $SUBSCRIPTION_ID with your own subscription ID
+$SUBSCRIPTION_ID="replace here"
+
+terraform import azurerm_resource_group.rg '/subscriptions/$SUBSCRIPTION_ID/resourceGroups/myResourceGroup'
+
+# importing the resource group doesn't automatically import its child resources
+# import the storage account
+terraform import azurerm_storage_account.storage '/subscriptions/$SUBSCRIPTION_ID/resourceGroups/myResourceGroup/providers/Microsoft.Storage/storageAccounts/tfsademo01'
 
 # check the content of the terraform.state file to notice a new section for the RG
 # starting from now, we can manage this resource group using terraform
@@ -17,8 +25,4 @@ terraform import azurerm_resource_group.rg '/subscriptions/<SUBSCRIPTION_ID>/res
 # plan and preview terraform changes
 terraform plan
 
-# deploy terraform infra
-terraform apply
-
-# destroy infra
-terraform destroy
+az group delete -g myResourceGroup
--- 90_import_resource/main.tf
+++ 90_import_resource/main.tf
@@ -1,5 +1,14 @@
 # resource group in Azure
 resource "azurerm_resource_group" "rg" {
-  name     = "myResourceGroup2"
+  name     = "myResourceGroup"
   location = "westeurope"
+}
+
+# storage account in Azure
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfsademo01" # replace with unique name
+  resource_group_name      = "myResourceGroup"
+  location                 = "westeurope"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
