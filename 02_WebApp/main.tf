ded239f5966c5f51ece11373f5ffeec6d8b9a43c
hassene fliss <hassen.fliss@avaxia-group.com>
2022-02-03 06:33:56 +0900

    ignore provider

--- 02_WebApp/main.tf
+++ 02_WebApp/main.tf
@@ -1,6 +1,7 @@
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.resource_group_location
+  
 }
 
 resource "azurerm_app_service_plan" "example" {
