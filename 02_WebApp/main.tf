94c24cb0cfb8189fa5b1bd456db99ce723774cc7
hassene fliss <hassen.fliss@avaxia-group.com>
2022-02-03 06:59:55 +0900

    test

--- 02_WebApp/main.tf
+++ 02_WebApp/main.tf
@@ -1,13 +1,8 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-
-}
 
 resource "azurerm_app_service_plan" "example" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = var.resource_group_name
 
   sku {
     tier = "Standard"
