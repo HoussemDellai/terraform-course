9051167890cf31ee9ae7f740810d155f2f7c90b1
hassene fliss <hassen.fliss@avaxia-group.com>
2022-02-17 00:08:34 +0900

    test

--- 01_ResourceGroup/main.tf
+++ 01_ResourceGroup/main.tf
@@ -3,10 +3,10 @@
 terraform {
 
   required_version = ">=0.12"
-  
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
   }
@@ -18,5 +18,5 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "az104"
-  location  = var.resource_group_location
+  location = var.resource_group_location
 }
