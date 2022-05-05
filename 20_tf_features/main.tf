f57802756153924df530f8eacdb2df5d9c4643e6
Houssem Dellai <houssem.dellai@live.com>
2022-05-05 22:04:27 +0200

    added demo for dynamic

--- 20_tf_features/main.tf
+++ 20_tf_features/main.tf
@@ -106,3 +106,57 @@ resource "random_uuid" "demo08_random_id" {}
 output "demo08_random_id" {
   value = random_uuid.demo08_random_id.result
 }
+
+#------------------------------------------------------
+# dynamic
+#------------------------------------------------------
+
+# https://www.terraform.io/language/expressions/dynamic-blocks
+
+locals {
+  subnets = [{
+    subnet_name           = "subnet1"
+    subnet_address_prefix = "10.0.1.0/24"
+    },
+    {
+      subnet_name           = "subnet2"
+      subnet_address_prefix = "10.0.2.0/24"
+  }]
+}
+
+resource "azurerm_resource_group" "demo07_rg" {
+  name     = "demo07_rg"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-network"
+  location            = azurerm_resource_group.demo07_rg.location
+  resource_group_name = azurerm_resource_group.demo07_rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  dynamic "subnet" {
+    for_each = local.subnets
+    content {
+      name           = subnet.value.subnet_name
+      address_prefix = subnet.value.subnet_address_prefix
+    }
+  }
+
+  # subnet {
+  #   name           = "subnet1"
+  #   address_prefix = "10.0.1.0/24"
+  # }
+
+  # subnet {
+  #   name           = "subnet2"
+  #   address_prefix = "10.0.2.0/24"
+  # }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.demo07_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
