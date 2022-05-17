688288ac948e6ba4c3de5909a22694b45f81760f
Houssem Dellai <houssem.dellai@live.com>
2022-05-17 14:56:42 +0200

    added demo 95 for migrating deprecated resources

--- 95_migrate_deprecated/deprecated.tf
+++ 95_migrate_deprecated/deprecated.tf
@@ -9,7 +9,6 @@ resource "azurerm_app_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
-  reserved            = true
 
   sku {
     tier = "Standard"
