a8fddf23b993716dc74c5c3236b5c351dc058a40
Houssem Dellai <houssem.dellai@live.com>
2022-05-19 13:06:12 +0200

    added demo 91 for using aztfy

---
+++ 91_import_terrafy/commands.sh
@@ -0,0 +1,11 @@
+# Install Azure Terrafy
+go install github.com/Azure/aztfy@latest
+
+# Azure Terrafy commands
+aztfy
+
+# Azure Terrafy needs an empty folder to export config
+mkdir terraform-web-sql
+
+# Generate Terraform configuration and state file
+aztfy -o terraform-web-sql rg-terraform-web-sql-db
