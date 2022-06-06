4bef61cdeb5670590b9e177b5daa614d9c5b0dce
Houssem Dellai <houssem.dellai@live.com>
2022-06-06 15:57:36 +0200

    configured grafana & prometheus

--- 110_helm_provider/helm_charts.tf
+++ 110_helm_provider/helm_charts.tf
@@ -3,28 +3,98 @@ resource "helm_release" "pod_identity" {
   repository = "https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts"
   chart      = "aad-pod-identity"
   namespace  = "kube-system"
-  create_namespace = true
 }
 
-resource "helm_release" "nginx_ingress" {
+# https://github.com/kubernetes/ingress-nginx/tree/main/charts/ingress-nginx
+resource "helm_release" "nginx_ingress_controller" {
   name             = "nginx-ingress-controller"
-  repository       = "https://charts.bitnami.com/bitnami"
-  chart            = "nginx-ingress-controller"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.1.3"
   namespace        = "ingress"
-  create_namespace = true
+  create_namespace = "true"
 
   set {
-    name  = "service.type"
-    value = "ClusterIP"
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "controller.autoscaling.enabled"
+    value = "true"
+  }
+  set {
+    name  = "controller.autoscaling.minReplicas"
+    value = "2"
+  }
+  set {
+    name  = "controller.autoscaling.maxReplicas"
+    value = "10"
   }
 }
 
-resource "helm_release" "prometheus" {
-  name             = "prometheus"
+# resource "helm_release" "nginx_ingress" {
+#   name             = "nginx-ingress-controller"
+#   repository       = "https://charts.bitnami.com/bitnami"
+#   chart            = "nginx-ingress-controller"
+#   namespace        = "nginx-ingress"
+#   create_namespace = true
+
+#   set {
+#     name  = "service.type"
+#     value = "LoadBalancer"
+#   }
+# }
+
+# https://github.com/prometheus-community/helm-charts/tree/main/charts/kube-prometheus-stack
+resource "helm_release" "prometheus_stack" {
+  name             = "prometheus-stack"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
   namespace        = "monitoring"
   create_namespace = true
+
+  provisioner "local-exec" {
+    command = "kubectl describe ingress prometheus-stack-grafana -n monitoring >> ingress.txt"
+  }
+
+  set {
+    name  = "grafana.ingress.enabled"
+    value = "true"
+  }
+  set {
+    name  = "grafana.ingress.ingressClassName"
+    value = "nginx"
+  }
+  set {
+    name  = "grafana.ingress.path"
+    value = "/(.*)" # "/grafana2/?(.*)"
+  }
+  # annotations:
+  #   nginx.ingress.kubernetes.io/ssl-redirect: "false"
+  #   nginx.ingress.kubernetes.io/use-regex: "true"
+  #   nginx.ingress.kubernetes.io/rewrite-target: /$1
+  set {
+    name  = "grafana.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/ssl-redirect"
+    value = "false"
+    type  = "string"
+  }
+  set {
+    name  = "grafana.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/use-regex"
+    value = "true"
+    type  = "string"
+  }
+  set {
+    name  = "grafana.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/rewrite-target"
+    value = "/$1"
+  }
+  set {
+    name = "grafana.adminUser"
+    value = "grafana"
+  }
+  set {
+    name = "grafana.adminPassword"
+    value = "grafana-pass"
+  }
 }
 
 resource "helm_release" "argo_cd" {
@@ -36,7 +106,7 @@ resource "helm_release" "argo_cd" {
   create_namespace = true
 
   set {
-    name = "server.service.type"
+    name  = "server.service.type"
     value = "LoadBalancer"
   }
 }
