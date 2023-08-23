07f767e250d539756b2ec6d13465681a85028787
Houssem Dellai <hodellai@microsoft.com>
2023-08-23 16:54:18 +0200

    130_module_dependencies

--- 130_module_dependencies/main.tf
+++ 130_module_dependencies/main.tf
@@ -21,21 +21,21 @@ module "storage_account" {
 
 # Scenario 2: module storage_account depends explicitly on module keyvault
 
-module "storage_account" {
-  source = "./modules/storage_account"
+# module "storage_account" {
+#   source = "./modules/storage_account"
 
-  storage_account_name = "strg1235790"
-  resource_group_name  = azurerm_resource_group.rg.name
+#   storage_account_name = "strg1235790"
+#   resource_group_name  = azurerm_resource_group.rg.name
 
-  depends_on = [ module.keyvault ] # explicit dependency on entire module
-}
+#   depends_on = [ module.keyvault ] # explicit dependency on entire module
+# }
 
 # Scenario 3: module storage_account depends implicitly on module keyvault
 # Depends only on the public IP from module keyvault
 
-module "storage_account" {
-  source = "./modules/storage_account"
+# module "storage_account" {
+#   source = "./modules/storage_account"
 
-  storage_account_name = module.keyvault.key_vault_name # implicit dependency on key_vault_name output
-  resource_group_name  = azurerm_resource_group.rg.name
-}
+#   storage_account_name = module.keyvault.key_vault_name # implicit dependency on key_vault_name output
+#   resource_group_name  = azurerm_resource_group.rg.name
+# }
